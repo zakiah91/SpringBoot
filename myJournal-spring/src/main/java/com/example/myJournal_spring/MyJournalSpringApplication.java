8cb05aec6eda8a3cc74c2b113c0282078a3178d0
zakiah91 <107566944+zakiah91@users.noreply.github.com>
2024-11-02 18:51:43 +0800

    Generate war file to solve solve issue #3
    Generate war file

--- myJournal-spring/src/main/java/com/example/myJournal_spring/MyJournalSpringApplication.java
+++ myJournal-spring/src/main/java/com/example/myJournal_spring/MyJournalSpringApplication.java
@@ -6,12 +6,13 @@ import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MyJournalSpringApplication  {
+public class MyJournalSpringApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyJournalSpringApplication.class, args);
